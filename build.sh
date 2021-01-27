be838540a046eadf20085baf1832a7725672ea96
Ivan Buetler <ivan.buetler@compass-security.com>
2021-01-27 11:43:06 +0100

    update

--- build.sh
+++ build.sh
@@ -1,6 +1,3 @@
+#!/bin/bash
 docker build --no-cache -t hackinglab/alpine-ntpd:3.2.0 -t hackinglab/alpine-ntpd:3.2 -t hackinglab/alpine-ntpd:latest -f Dockerfile .
-
-docker push hackinglab/alpine-ntpd
-docker push hackinglab/alpine-ntpd:3.2
-docker push hackinglab/alpine-ntpd:3.2.0
 
